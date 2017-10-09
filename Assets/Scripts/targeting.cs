35846f3cca803d196c6c774a747bf060f40a1fa0
Andrew Desktop <anda02@gmail.com>
2017-10-09 11:14:39 -0400

    Initial

---
+++ Assets/Scripts/targeting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targeting : MonoBehaviour {
+
+	public GameObject p1;
+	public GameObject p2;
+	public GameObject p3;
+	public GameObject p4;
+	public GameObject p5;
+	public GameObject p6;
+	public GameObject p7;
+	public GameObject p8;
+	public GameObject p9;
+	public GameObject p10;
+	public GameObject p11;
+	public GameObject p12;
+	public GameObject p13;
+	GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
